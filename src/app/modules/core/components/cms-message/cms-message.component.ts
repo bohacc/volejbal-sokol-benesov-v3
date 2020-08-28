824edb768ffa31a1143740812829e852eac0c1ce
Martin Boháč <bohacc@seznam.cz>
2020-08-28 23:06:30 +0200

    css template

--- src/app/modules/core/components/cms-message/cms-message.component.ts
+++ src/app/modules/core/components/cms-message/cms-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cms-message',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cms-message.component.scss']
 })
 export class CmsMessageComponent implements OnInit {
+  @Input() date: Date;
+  @Input() title: string;
+  @Input() image: string;
+  @Input() imgCyrcle: boolean;
 
   constructor() { }
 
